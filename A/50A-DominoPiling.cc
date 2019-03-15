309f2483b917ffdda22046e8dde7b12edae9e899
Nicolas <nicolas.sabbatini.95@gmail.com>
2019-03-15 18:24:28 -0300

    50A-DominoPiling

---
+++ A/50A-DominoPiling.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int
+main(){
+    ios::sync_with_stdio(false);
+    int a, b, r;
+    cin >> a >> b;
+    r = (a*b)/2;
+    cout << r << endl;
+    return 0;
+}
